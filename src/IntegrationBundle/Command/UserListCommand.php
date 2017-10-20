c2f47f42510db449f65982b5758fc784bc59e748
Philip Washington Sorst <philip@sorst.net>
2017-10-20 17:56:15 +0200

    Adding user commands

---
+++ src/IntegrationBundle/Command/UserListCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace IntegrationBundle\Command;
+
+use DomainBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+class UserListCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('app-platform:users:list');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userRepository = $this->getContainer()->get('doctrine')->getRepository(User::class);
+        /** @var User[] $users */
+        $users = $userRepository->findAll();
+
+        $table = new Table($output);
+        $table->setHeaders(['username', 'email', 'roles', 'active']);
+        foreach ($users as $user) {
+            $table->addRow(
+                [$user->getUsername(), $user->getEmail(), implode(',', $user->getRoles()), $user->isEnabled() ?? 'x']
+            );
+        }
+        $table->render();
+    }
+}
